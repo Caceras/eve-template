593a801da523782a732d7a93afe0c44cc5a5bb51
Riki Caceras <38214934+Caceras@users.noreply.github.com>
2026-09-22 01:21:58 +0200

    brand: add reusable Aegentica image mark

---
+++ apps/templates/eve-template/app/_components/aegentica-image-mark.tsx
@@ -0,0 +1,16 @@
+export function AegenticaImageMark({ size }: { readonly size: number }) {
+  return (
+    <svg
+      fill="none"
+      height={size}
+      viewBox="0 0 1254 1254"
+      width={size}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M278 810 L357 811 L489 642 L601 642 L602 811 L956 811 L956 758 L665 757 L666 642 L956 642 L956 589 L532 588 L627 467 L956 467 L956 416 L585 416 Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
